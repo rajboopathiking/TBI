f7ae7fcbc271f4e6a49fcd26ae6ad7e3391e742b
godfather101 <godfather101@godfather101.kali>
2024-04-30 17:38:01 +0530

    add files

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -6,7 +6,7 @@ export default function Contact() {
             <div className='m-10  bg-purple-300 p-2 py-10 rounded-[20px]'>
             <h1 className='font-bold relative left-[15%] text-[30px] flex m-2  text-black'>Contact</h1>
             <p className='mx-10 sm:hidden text-black'>For Any Queries Contact via Mail</p>
-                <form action="mailto:{}">
+                <form action="mailto:{boopathiraj.researchbrains@gmail.com}">
                     <div>
                         <label htmlFor="Username">
                             <input className='border border-black rounded h-10 m-2 p-2 sm:w-[90%]' type="text" id='Username' placeholder='Username'/>

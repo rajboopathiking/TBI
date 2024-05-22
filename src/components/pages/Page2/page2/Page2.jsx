484f3a9146863d20d682a76f47e1718894e8a740
godfather101 <godfather101@godfather101.kali>
2024-05-22 13:08:02 +0530

    add files

--- src/components/pages/Page2/page2/Page2.jsx
+++ src/components/pages/Page2/page2/Page2.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import Navbar from '../../page1/Navbar/Navbar'
 import Footer from '../../page1/Footer/Footer'
-import Content2 from '../Content2/Content2'
 import Content from "../Content/Content"
 
 export default function Page2() {
